e75714f337c3b7f0b010de643a06346df31308fe
Williamtt33 <Williamtt33@users.noreply.github.com>
2026-06-27 22:20:48 +0800

    fix: switch to HashRouter for GitHub Pages SPA compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
 import ErrorBoundary from './components/ErrorBoundary'
 import App from './App'
@@ -9,10 +9,10 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter basename={import.meta.env.PROD ? '/gs-showcase' : '/'}>
+      <HashRouter>
         <ScrollToTop />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
   </React.StrictMode>,
 )
